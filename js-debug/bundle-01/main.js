0a419d143b17255c69cac5f049fd95d92dce6668
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-10 16:19:35 +0100

    add: bundle 1

--- js-debug/bundle-01/main.js
+++ js-debug/bundle-01/main.js
@@ -11,37 +11,46 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
-    console.log(i);
+for (let i = 0; i < 5; i++) { // < al posto di >
+    // console.log(i);
 }
-
 
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {      //if (num % 2 = 0) {
         return num + 5;
     }
     return num;
 }
 
+//console.log(addIfEven(7)); // Output: 7
+//console.log(addIfEven(10)); // Output: 15
+
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {     //; al posto di ,
         console.log(i);
     }
 }
+// console.log(loopToFive());
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {  //tolto; dopo i++  e tolto -1 dopo member.lenght
+        if (numbers[i] % 2 === 0); {   //=== AL POSTO DI =  aggiunto numers[i]
+            numbers.push(evenNumbers[i]); //push in evenNumbers[i]
         }
-        return evenNumbers;
-    }
+        return numbers;
+    } 
+       
 }
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+const result = displayEvenNumbers();
+console.log(result); 
+
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
